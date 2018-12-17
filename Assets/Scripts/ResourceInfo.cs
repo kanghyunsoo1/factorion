7deeed2457d4912f8b42b19d1ced40a0b2d6d8b6
kanghyunsoo <dmhskang@naver.com>
2018-12-18 02:54:45 +0900

    Signed-off-by: kanghyunsoo <dmhskang@naver.com>

---
+++ Assets/Scripts/ResourceInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class ResourceInfo
+{
+    public int resId;
+    public string resName;
+    public int minAmount;
+    public int maxAmount;
+    public int rangeFactor;
+    public float chance;
+    public float minRange;
+}
