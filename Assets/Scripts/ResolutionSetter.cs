d91fcd1bb1b1a827fa90396ad34d350f1743b977
kanghyunsoo <dmhskang@naver.com>
2018-12-21 21:30:32 +0900

    화면 해상도 통일, 스크립트 실행순서 변경

---
+++ Assets/Scripts/ResolutionSetter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionSetter : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        Screen.SetResolution(720, 1280, true);
+	}
+	
+}
