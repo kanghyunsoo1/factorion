ce0ea08cb7c139c9906c1a136fed757558170f58
kanghyunsoo <dmhskang@naver.com>
2019-01-05 09:55:11 +0900

    격자 이펙트 추가

---
+++ Assets/Scripts/Managers/NewGridEffectManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGridEffectManager :MonoBehaviour {
+    public GameObject Grid;
+    public float Delay;
+    public float LifeTime;
+
+    private Camera cam;
+    private float width = 18;
+    private float height = 30;
+    private bool isAdd = false;
+    void Start() {
+        cam = Camera.main;
+        StartCoroutine(Loop());
+    }
+
+
+    IEnumerator Loop() {
+        while (true) {
+            yield return new WaitForSeconds(LifeTime * 4);
+            var cx = cam.transform.position.x;
+            var cy = cam.transform.position.y;
+            for (float y = height; y >= -height; y--) {
+                yield return new WaitForSeconds(Delay);
+                for (float x = -width; x <= width; x++) {
+
+                    var go = (GameObject)Instantiate(Grid);
+                    go.transform.position = new Vector3(Mathf.Floor(cx + x), Mathf.Floor(cy + y), 0);
+                    go.GetComponent<NewGrid>().LifeTime = LifeTime;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NewGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGrid :MonoBehaviour {
+    public float LifeTime;
+    private SpriteRenderer renderer;
+    private Color color;
+    private bool isUp = true;
+    void Start() {
+        color = new Color(1f, 1f, 1f, 0f);
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.color = color;
+        Destroy(gameObject, LifeTime);
+    }
+
+    void FixedUpdate() {
+        if (isUp) {
+            color.a += Time.deltaTime / LifeTime * 2;
+            if (color.a >= 1f) {
+                color.a = 1f;
+                isUp = false;
+            }
+        } else {
+            color.a -= Time.deltaTime / LifeTime * 2;
+        }
+
+        renderer.color = color;
+    }
+}
