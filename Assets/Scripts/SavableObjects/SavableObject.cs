e029bff0bcf00e1ad884e651a54747c7a761809e
kanghyunsoo <dmhskang@naver.com>
2019-01-05 10:20:59 +0900

    서식 맞춤

--- Assets/Scripts/SavableObjects/SavableObject.cs
+++ Assets/Scripts/SavableObjects/SavableObject.cs
@@ -3,30 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SavableObject : MonoBehaviour
-{
+public class SavableObject :MonoBehaviour {
     public Vector3 position;
     public Vector3 rotation;
-    public void SaveTransform()
-    {
+    public void SaveTransform() {
         position = transform.position;
         rotation = transform.rotation.eulerAngles;
     }
-    public void LoadTransform()
-    {
+    public void LoadTransform() {
         transform.position = position;
         transform.rotation = Quaternion.Euler(rotation);
     }
-    public void BeforeSave()
-    {
+    public void BeforeSave() {
         SaveTransform();
     }
-    public void AfterLoad()
-    {
+    public void AfterLoad() {
         LoadTransform();
     }
-    public string ToJson()
-    {
-        return JsonUtility.ToJson(this, true);
+    public string ToJson() {
+        return JsonUtility.ToJson(this);
     }
 }
