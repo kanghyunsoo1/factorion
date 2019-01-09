aee560fc2f974a349e491b1ff8db7cba7dbafb26
kanghyunsoo <dmhskang@naver.com>
2019-01-09 21:36:35 +0900

    UpgradeableValue

---
+++ Assets/Scripts/Game/Data/UpgradableValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradableValue :SavableObject {
+    public string valueName;
+    public float defaultValue;
+    public float deltaValue;
+    public int upgradeCount;
+    public int maxUpgradeCount;
+    public int defaultPrice;
+    public int deltaPrice;
+    public Sprite sprite;
+
+    public float Value {
+        get {
+            return deltaValue * upgradeCount + defaultValue;
+        }
+    }
+    public float Price {
+        get {
+            return deltaPrice * upgradeCount + defaultPrice;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Data/ValueManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueManager :MonoBehaviour {
+
+    private UpgradableValue[] _values;
+    void Awake() {
+        _values = FindObjectsOfType<UpgradableValue>();
+    }
+
+    public void RefreshValues() {
+        _values = FindObjectsOfType<UpgradableValue>();
+    }
+
+    public UpgradableValue GetValue(string name) {
+        return Array.Find(_values, x => x.valueName.Equals(name));
+    }
+
+    public UpgradableValue[] GetUpgradableValues() {
+        return Array.FindAll(_values, x => x.maxUpgradeCount > 0);
+    }
+
+    public UpgradableValue[] GetUnupgradableValues() {
+        return Array.FindAll(_values, x => x.maxUpgradeCount == 0);
+    }
+}
