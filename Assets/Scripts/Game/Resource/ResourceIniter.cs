215d1b3f7246ecbadeaccf1aed6ba7727c04f9fe
kanghyunsoo <dmhskang@naver.com>
2019-01-16 17:45:24 +0900

    ResourceIniter

---
+++ Assets/Scripts/Game/Resource/ResourceIniter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceIniter :MonoBehaviour {
+
+    private ResourceManager _rm;
+    private Resource _resource;
+    public void Awake() {
+        _rm = FindObjectOfType<ResourceManager>();
+        _resource = GetComponent<Resource>();
+    }
+
+    public void Start() {
+        var ri = _rm.resourceInfos[_resource.resourceIndex];
+        GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/block_" + ri.name);
+        Destroy(this);
+    }
+}
