ecfd8503c0efb64ed81e74c879b466d2fa2cb7ae
kanghyunsoo <dmhskang@naver.com>
2019-02-08 22:31:01 +0900

    알림 추가

---
+++ Assets/Scripts/Game/GUI/AlertBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlertBox :MonoBehaviour {
+    public float myY;
+    private RectTransform _rectTransform;
+    private void Start() {
+        _rectTransform = GetComponent<RectTransform>();
+        Destroy(gameObject, 3f);
+    }
+    void Update() {
+        _rectTransform.anchoredPosition = Vector3.Lerp(_rectTransform.anchoredPosition, new Vector3(0f, myY, 0f), 0.5f);
+    }
+}
---
+++ Assets/Scripts/Game/GUI/AlertManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlertManager :MonoBehaviour {
+    public AlertBox alertBox;
+    private List<AlertBox> _list;
+    private TextManager _tm;
+    void Awake() {
+        _tm = GetComponent<TextManager>();
+        _list = new List<AlertBox>();
+    }
+
+    public void AddAlert(string textWithoutAlertHead, Color color) {
+        var a = Instantiate(alertBox).GetComponent<AlertBox>();
+        a.transform.SetParent(FindObjectOfType<Canvas>().transform);
+        var text = a.transform.Find("Text").GetComponent<Text>();
+        text.text = _tm.GetText("alert", textWithoutAlertHead);
+        text.color = color;
+        _list.Insert(0, a);
+        for (int i = 0; i < _list.Count; i++) {
+            var aa = _list[i];
+            aa.myY = -200 - 50 * i;
+        }
+    }
+}
