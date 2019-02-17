7bab763099c95437ba744a6d4048a16a15548cf0
kanghyunsoo <dmhskang@naver.com>
2019-02-17 16:08:42 +0900

    KhsUtil 추가

---
+++ Assets/Scripts/Game/Khs/KhsUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class KhsUtil {
+
+    public static T GetNearestObject<T>(T[] array, Vector2 pos, Predicate<T> match) where T : MonoBehaviour {
+        float minDistance = 100f;
+        T result = null;
+        foreach (var obj in array) {
+            if (!match(obj))
+                continue;
+            var distance = Vector2.Distance(pos, obj.transform.position);
+            if (distance < minDistance) {
+                minDistance = distance;
+                result = obj;
+            }
+        }
+        return result;
+    }
+
+    public static T GetNearestObject<T>(T[] array, Vector2 pos) where T : MonoBehaviour => GetNearestObject<T>(array, pos, x => true);
+}
