fb04d4a1dda0284f55bb64d7231c85b391c3cf92
kanghyunsoo <dmhskang@naver.com>
2019-02-25 20:49:14 +0900

    코드 정리

--- Assets/Scripts/Game/Camera/ResolutionSetter.cs
+++ Assets/Scripts/Game/Camera/ResolutionSetter.cs
@@ -1,11 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class ResolutionSetter :MonoBehaviour {
-
     void Awake() {
         Screen.SetResolution(720, 1280, true);
     }
-
 }
