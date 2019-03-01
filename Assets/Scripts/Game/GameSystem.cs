d86d06d7ce23637964cd94b43d7420e418fb740c
kanghyunsoo <dmhskang@naver.com>
2019-03-01 20:29:50 +0900

    디버그용으로 있던 치트 제거

--- Assets/Scripts/Game/GameSystem.cs
+++ Assets/Scripts/Game/GameSystem.cs
@@ -17,17 +17,4 @@ public class GameSystem :MonoBehaviour {
         _spawnManager.Spawn();
         _dataManager.Load();
     }
-
-    private void FixedUpdate() {
-        var i = FindObjectOfType<Warehouse>().GetComponent<Inventory>();
-        switch (Random.Range(0, 100)) {
-            case 0: i.AddItem("iron", 1); break;
-            case 1: i.AddItem("iron-bar", 1); break;
-            case 2: i.AddItem("coal", 1); break;
-            case 3: i.AddItem("copper", 1); break;
-            case 4: i.AddItem("copper-bar", 1); break;
-            case 5: i.AddItem("tin", 1); break;
-            case 6: i.AddItem("tin-bar", 1); break;
-        }
-    }
 }
